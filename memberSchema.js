29a720609ca8a04854a9617b9dbd97b8b875b639
ddanccarloss <ronaldallancarnate@yahoo.com>
2025-05-13 19:37:41 +0800

    Fix syntax

--- memberSchema.js
+++ memberSchema.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: 'postgresql://thehivedb_user:AJbm3i7RIniGFYUCWNRsei3jPJxjSoYn@dpg-d0hfp93e5dus73b076u0-a/thehivedb',, // Use your database connection string here
+    connectionString: 'postgresql://thehivedb_user:AJbm3i7RIniGFYUCWNRsei3jPJxjSoYn@dpg-d0hfp93e5dus73b076u0-a/thehivedb', // Use your database connection string here
     ssl: {
         rejectUnauthorized: false, // Required for Render free-tier PostgreSQL
     },
